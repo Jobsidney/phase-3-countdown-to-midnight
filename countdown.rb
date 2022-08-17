94457914ac63ce6a8b5843946a6ecfc7a6f966cf
Jobsidney <jobsidney67@gmail.com>
2022-08-17 16:29:33 +0300

    commit tree 855c8bc3a12ba650bf389dae8c925b5b0df51b58

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+
+def countdown_with_sleep(inte)
+    sleep(inte)
 end
+
+def countdown(integ)
+    while integ>0
+        puts "#{integ} SECOND(S)!"
+        integ -=1
+        countdown_with_sleep(0.5)
+    end
+    "HAPPY NEW YEAR!"
+end
+countdown(12)
+
